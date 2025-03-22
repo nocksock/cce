05fc3adb4e1b9ffcfee243e22bb62a7717b6a1ed
Nils Riedemann <nils@bleepbloop.studio>
2025-03-22 20:24:21 +0100

    refactor(events): use isString predicate over typeof

--- lib/events.ts
+++ lib/events.ts
@@ -1,45 +1,68 @@
-import { invariant } from "./errors";
+import { invariant } from './errors'
+import { isFunction, isString } from './predicates'
 
-export const dispatch = <E extends string, T>(self: EventTarget, type: E, detail?: T) => {
-    let eventType: string;
+export const dispatch = <E extends string | Function, T>(
+  self: EventTarget,
+  type: E,
+  detail?: T
+) => {
+  let eventType: string
 
-    if (typeof type == "string" && !detail) {
-      eventType = type;
-      return self.dispatchEvent(new Event(eventType, { bubbles: true, composed: true }))
-    }
+  if (isString(type) && !detail) {
+    eventType = type
+    return self.dispatchEvent(
+      new Event(eventType, { bubbles: true, composed: true })
+    )
+  }
 
-    if (typeof type == "string" && detail) {
-      eventType = type;
-      return self.dispatchEvent(new CustomEvent(eventType, { detail, bubbles: true, composed: true }))
-    }
+  if (isString(type) && detail) {
+    eventType = type
+    return self.dispatchEvent(
+      new CustomEvent(eventType, { detail, bubbles: true, composed: true })
+    )
+  }
 
-    if (typeof type == "function")  {
-      invariant(type.name, "Cannot dispatch functions without a name (eg anonymous functions)")
-      eventType = type.name
-      self.dispatchEvent(new CustomEvent(type, { detail, bubbles: true, composed: true }))
-    }
+  if (isFunction(type)) {
+    invariant(
+      type.name,
+      'Cannot dispatch functions without a name (eg anonymous functions)'
+    )
+    eventType = type.name
+    self.dispatchEvent(
+      new CustomEvent(eventType, { detail, bubbles: true, composed: true })
+    )
+  }
 }
 
-
-function stopHandler(cb: (e: Event) => void, e: Event) {
+function stopHandler(cb: (e: Event | CustomEvent) => void, e: Event) {
   e.stopPropagation()
   cb(e)
 }
 
-export const addEventListener = <E extends string, T>(self: EventTarget, type: E, handler: (e: CustomEvent<T>) => void, stopPropagation?: true) => {
-  let event: string;
-  if (typeof type == "string") {
+export const addEventListener = <
+  E extends string | (Function & { name: string }),
+  T,
+>(
+  self: HTMLElement & { shadowRoot: ShadowRoot },
+  type: E,
+  handler: (e: Event | CustomEvent<T>) => void,
+) => {
+  let event: string
+  if (typeof type == 'string') {
     event = type
   }
 
-  if (typeof type == "function") {
-    invariant(type.name, "Cannot handle functions without a name (eg anonymous functions)")
+  if (typeof type == 'function') {
+    invariant(
+      isString(type.name),
+      'Cannot handle functions without a name (eg anonymous functions)'
+    )
     event = type.name
   }
 
   const boundHandler = stopHandler.bind(self, handler)
 
+  // @ts-ignore
   self.shadowRoot.addEventListener(event, boundHandler)
   return () => self.shadowRoot.removeEventListener(event, boundHandler)
 }
-
---
+++ lib/predicates.ts
@@ -0,0 +1,3 @@
+
+export const isString = (value: any): value is string => typeof value === "string";
+export const isFunction = (value: any): value is Function => typeof value === "function";
