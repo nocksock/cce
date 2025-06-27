514211fb9422357351d95ba1ac73963737256450
Nils Riedemann <nils@bleepbloop.studio>
2025-06-28 00:05:57 +0200

    fix: actually include ref in main bundle

--- lib/main.ts
+++ lib/main.ts
@@ -2,5 +2,6 @@ export * from "./context";
 export * from "./css";
 export * from "./custom-element";
 export * from "./cce";
+export * from "./directives";
 export { html } from "./html";
 export { signal, effect, computed } from "./signals";
